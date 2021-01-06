039d864b9f707abf7ec41aa1bb2e52a2e40aecd2
wscqtlulu <wscqtlulu@163.com>
2021-01-06 09:26:23 +0800

    二叉树：相同的树

---
+++ src/main/java/algorithm/tree/IsSameTree.java
@@ -0,0 +1,95 @@
+package algorithm.tree;
+
+//给定两个二叉树，编写一个函数来检验它们是否相同。
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+//
+// 示例 1:
+//
+// 输入:       1         1
+//          / \       / \
+//         2   3     2   3
+//
+//        [1,2,3],   [1,2,3]
+//
+//输出: true
+//
+// 示例 2:
+//
+// 输入:      1          1
+//          /           \
+//         2             2
+//
+//        [1,2],     [1,null,2]
+//
+//输出: false
+//
+//
+// 示例 3:
+//
+// 输入:       1         1
+//          / \       / \
+//         2   1     1   2
+//
+//        [1,2,1],   [1,1,2]
+//
+//输出: false
+//
+// Related Topics 树 深度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        } else if (p == null || q == null){
+            return false;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+        boolean left = false;
+        if (p.left != null && q.left != null){
+            left = isSameTree(p.left, q.left);
+        } else if (p.left == null && q.left == null){
+            left = true;
+        }
+        boolean right = false;
+        if (p.right != null && q.right != null){
+            right = isSameTree(p.right, q.right);
+        } else if (p.right == null && q.right == null){
+            right = true;
+        }
+        return left && right;
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
