b1d1e0d6f6221d15fa05b7bc624c02aacf42a5c3
wscqtlulu <wscqtlulu@163.com>
2021-01-23 17:06:01 +0800

    二叉树：翻转二叉树

---
+++ src/main/java/algorithm/tree/InvertTree.java
@@ -0,0 +1,70 @@
+package algorithm.tree;
+
+//翻转一棵二叉树。
+//
+// 示例：
+//
+// 输入：
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//
+// 输出：
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+// 备注:
+//这个问题是受到 Max Howell 的 原问题 启发的 ：
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+// Related Topics 树
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
