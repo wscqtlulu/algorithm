fb5a1cb57302214521d290f63f56c2c8539fbc57
wscqtlulu <wscqtlulu@163.com>
2021-01-25 14:55:36 +0800

    树：填充每个节点的下一个右侧节点指针

---
+++ src/main/java/algorithm/tree/Connect.java
@@ -0,0 +1,116 @@
+package algorithm.tree;
+
+//给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+//
+//
+//struct Node {
+//  int val;
+//  Node *left;
+//  Node *right;
+//  Node *next;
+//}
+//
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+//
+// 初始状态下，所有 next 指针都被设置为 NULL。
+//
+//
+//
+// 进阶：
+//
+//
+// 你只能使用常量级额外空间。
+// 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+//
+//
+//
+//
+// 示例：
+//
+//
+//
+//
+//输入：root = [1,2,3,4,5,6,7]
+//输出：[1,#,2,3,#,4,5,6,7,#]
+//解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由
+//next 指针连接，'#' 标志着每一层的结束。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数量少于 4096
+// -1000 <= node.val <= 1000
+//
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+//填充每个节点的下一个右侧节点指针
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Connect {
+    public Node connect(Node root) {
+        if (root == null){
+            return null;
+        }
+        connect(root.left, root.right);
+        return root;
+    }
+
+    public void connect(Node n1, Node n2){
+        if (n1 != null){
+            n1.next = n2;
+            connect(n1.left, n1.right);
+        }
+        if (n2 != null){
+            connect(n1.right, n2.left);
+            connect(n2.left, n2.right);
+        }
+    }
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
