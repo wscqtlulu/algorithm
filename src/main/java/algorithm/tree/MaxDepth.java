7aa6daeca8c3ba62106de705114c82e2e96b418d
wscqtlulu <wscqtlulu@163.com>
2021-01-31 00:25:00 +0800

    二叉树：二叉树的最大深度

---
+++ src/main/java/algorithm/tree/MaxDepth.java
@@ -0,0 +1,67 @@
+package algorithm.tree;
+
+//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例：
+//给定二叉树 [3,9,20,null,null,15,7]，
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+// 返回它的最大深度 3 。
+// Related Topics 树 深度优先搜索 递归
+// 👍 783 👎 0
+
+
+//二叉树的最大深度
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaxDepth {
+    int res = 0;
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, res);
+    }
+
+    public int maxDepth(TreeNode root, int res) {
+        if (root == null){
+            return res;
+        }
+        res++;
+        return Math.max(maxDepth(root.left, res), maxDepth(root.right, res));
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
