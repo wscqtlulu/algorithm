6082b65530aa925820befd7c7dd3bb1f3302ab58
wscqtlulu <wscqtlulu@163.com>
2021-02-07 22:08:00 +0800

    链表：反转链表（更新递归方法）

--- src/main/java/algorithm/dynamicProgramming/Rob3.java
+++ src/main/java/algorithm/dynamicProgramming/Rob3.java
@@ -64,12 +64,14 @@ class Rob3 {
         memo.put(root, res);
         return res;
     }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 }
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
