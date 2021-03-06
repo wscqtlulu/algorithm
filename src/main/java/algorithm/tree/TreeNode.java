ca7e6ea28f731e786011876865d8e8d692fae3c3
wscqtlulu <wscqtlulu@163.com>
2021-03-06 21:49:52 +0800

    合并二叉树

---
+++ src/main/java/algorithm/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package algorithm.tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
