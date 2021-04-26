c6a40b66d57d6dfa43330dcbcece127a23570893
wscqtlulu <wscqtlulu@163.com>
2021-04-26 22:53:56 +0800

    FizzBuzz

---
+++ src/main/java/algorithm/others/FizzBuzz.java
@@ -0,0 +1,59 @@
+package algorithm.others;
+
+//写一个程序，输出从 1 到 n 数字的字符串表示。
+//
+// 1. 如果 n 是3的倍数，输出“Fizz”；
+//
+// 2. 如果 n 是5的倍数，输出“Buzz”；
+//
+// 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+//
+// 示例：
+//
+// n = 15,
+//
+//返回:
+//[
+//    "1",
+//    "2",
+//    "Fizz",
+//    "4",
+//    "Buzz",
+//    "Fizz",
+//    "7",
+//    "8",
+//    "Fizz",
+//    "Buzz",
+//    "11",
+//    "Fizz",
+//    "13",
+//    "14",
+//    "FizzBuzz"
+//]
+//
+// 👍 95 👎 0
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add("" + i);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
